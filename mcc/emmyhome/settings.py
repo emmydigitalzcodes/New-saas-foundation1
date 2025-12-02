42ecaae93a34c6422e2c7aee672f436c9677f73a
emmydigtalzcodes <moseschukwuemeka07@gmail.com>
2025-12-02 13:35:27 +0100

    Added Django Debug environ var

--- mcc/emmyhome/settings.py
+++ mcc/emmyhome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5fo3cv4g_!qmbh-yey@2@boj1i4dmdd+yv+n1balktm#_l^x(c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.getenv("DEBUG")).lower() == "true"
+
+print("DEBUG:", DEBUG, type(DEBUG))
+
 
 ALLOWED_HOSTS = [
     ".railway.app"
