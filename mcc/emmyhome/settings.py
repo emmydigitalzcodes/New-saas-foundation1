552a69309654a6040077dddbae6052e7a7946a55
emmydigtalzcodes <moseschukwuemeka07@gmail.com>
2025-12-02 15:17:36 +0100

    Postgress database added

--- mcc/emmyhome/settings.py
+++ mcc/emmyhome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5fo3cv4g_!qmbh-yey@2@boj1i4dmdd+yv+n1balktm#_l^x(c'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.getenv("DEBUG")).lower() == "true"
+#DEBUG = str(os.getenv("DEBUG")).lower() == "true"
+
+DEBUG = config("DJANGO_DEBUG", cast=bool)
 
 print("DEBUG:", DEBUG, type(DEBUG))
 
@@ -91,6 +93,20 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
+DATABASE_URL = config("DATABASE_URL", cast=str)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=CONN_MAX_AGE,
+        conn_health_checks=True,
+        
+        )
 }
 
 
