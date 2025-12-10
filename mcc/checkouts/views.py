3225424d92ef11b7b75a544c0e292754dba7b7c9
emmydigtalzcodes <moseschukwuemeka07@gmail.com>
2025-12-10 05:09:51 +0100

    checkouts

--- mcc/checkouts/views.py
+++ mcc/checkouts/views.py
@@ -2,12 +2,14 @@ import helpers.billing
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required 
-
+from django.contrib.auth import get_user_model
 from django.conf import settings
+from django.http import HttpResponseBadRequest
 
 
-from subscriptions.models import SubscriptionPrice, Subscription
+from subscriptions.models import SubscriptionPrice, Subscription, UserSubscription
 
+User = get_user_model()
 BASE_URL = settings.BASE_URL
 
 # Create your views here.
@@ -55,5 +57,22 @@ def checkout_finalize_view(request):
         sub_obj = Subscription.objects.get(subscriptionprice__stripe_id=plan_id,)
     except:
         sub_obj = None
+    try:
+        user_obj = User.objects.get(customer__stripe_id=customer_id)
+    except:
+        user_obj = None
+    _user_sub_exists = False
+    try:
+        _user_sub_obj = UserSubscription.objects.get(user=user_obj)
+        _user_sub_exists = True
+    except UserSubscription.DoesNotExist:
+        _user_sub_obj = UserSubscription.objects.create(user=user_obj, subscription=sub_obj)
+    except:
+        _user_sub_obj = None
+    if None in [sub_obj, user_obj, _user_sub_obj]:
+        return HttpResponseBadRequest("There was an errorwith your account please contact us.")
+    if _user_sub_exists:
+        _user_sub_obj.subscription = sub_obj
+        _user_sub_obj.save()
     context = {}
     return render(request, "checkout/success.html", context)
